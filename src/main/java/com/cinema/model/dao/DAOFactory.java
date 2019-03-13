363428dbd4fadb922e9a8e13e013c955e9ffd55c
Anton Spasskikh <anton_spasskikh@epam.com>
2019-03-13 18:23:42 +0200

    supplier interface added to factory class

--- src/main/java/com/cinema/model/dao/DAOFactory.java
+++ src/main/java/com/cinema/model/dao/DAOFactory.java
@@ -5,6 +5,8 @@ import com.cinema.util.constants.DAOKey;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static com.cinema.util.constants.DAOKey.*;
 
@@ -18,17 +20,17 @@ public class DAOFactory {
     /**
      * map for instances
      */
-    private static Map<DAOKey, AbstractDAO> daoMap;
+    private static Map<DAOKey, Supplier<AbstractDAO>> daoMap;
 
     static {
         daoMap = new HashMap<>();
-        daoMap.put(USER_ROLE_DAO, new UserRoleDAO());
-        daoMap.put(USER_DAO, new UserDAO());
-        daoMap.put(MOVIE_DAO, new MovieDAO());
-        daoMap.put(SEAT_DAO, new SeatDAO());
-        daoMap.put(TIMESLOT_DAO, new TimeSlotDAO());
-        daoMap.put(SHOWTIME_DAO, new ShowTimeDAO());
-        daoMap.put(ORDER_DAO, new OrderDAO());
+        daoMap.put(USER_ROLE_DAO, UserRoleDAO::new);
+        daoMap.put(USER_DAO, UserDAO::new);
+        daoMap.put(MOVIE_DAO, MovieDAO::new);
+        daoMap.put(SEAT_DAO, SeatDAO::new);
+        daoMap.put(TIMESLOT_DAO, TimeSlotDAO::new);
+        daoMap.put(SHOWTIME_DAO, ShowTimeDAO::new);
+        daoMap.put(ORDER_DAO, OrderDAO::new);
     }
 
     /**
@@ -38,6 +40,6 @@ public class DAOFactory {
      * @return dao entity
      */
     public static AbstractDAO getDAO(DAOKey key) {
-        return daoMap.get(key);
+        return daoMap.get(key).get();
     }
 }
