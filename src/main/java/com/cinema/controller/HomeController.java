39b19f2e34bee6ca92c7303189d5799fe5d9a8f1
Anton Spasskikh <anton_spasskikh@epam.com>
2019-03-27 19:20:48 +0200

    home controller added

---
+++ src/main/java/com/cinema/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Welcome to Best Cinema!");
+        return "welcome";
+    }
+}
