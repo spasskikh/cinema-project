bf76ce377b5061f7b4793dd5f572151aefc4f6c3
Anton Spasskikh <anton_spasskikh@epam.com>
2019-03-29 17:00:30 +0200

    unnecessary field removed

--- src/main/java/com/cinema/controller/HomeController.java
+++ src/main/java/com/cinema/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.cinema.controller;
 
-import com.cinema.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-
-    @Autowired
-    private MovieService movieService;
 
     @RequestMapping("/")
     public String welcome() {
