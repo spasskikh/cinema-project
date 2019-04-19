493bb6dadd984e12fd71fa08debe4e6fcab40d55
Anton Spasskikh <anton_spasskikh@epam.com>
2019-04-19 12:17:59 +0300

    unnecessary initializer removed

--- src/main/java/com/cinema/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.cinema.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
